363e5da624db7fe3d7b828198f82a5bdbf6784e0
Antoine-Girard <antoine.girard@epitech.eu>
2023-11-21 20:46:09 +0100

    [ADD]: menu working

--- src/menu.gd
+++ src/menu.gd
@@ -1,28 +1,20 @@
 extends Control
-const Player = preload("./player.gd")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	pass  # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
+#func _process(delta):
+#   pass
 
 func _on_start_button_pressed():
-	var player_instance = Player.new()
-	player_instance._ready()
-	add_child(player_instance)
-	player_instance.start_player()
+	get_tree().change_scene_to_file("res://src/world.tscn")
 	pass
-
 
 func _on_options_button_pressed():
 	pass
 
-
 func _on_quit_button_pressed():
 	get_tree().quit()
-	pass
+	pass  # Replace with function body.
