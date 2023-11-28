68486f9e4d2817e20cd9922bd6eece65db1c24a4
kepalas02 <arthur.bourdin@epitech.eu>
2023-11-28 20:38:21 +0100

    [ADD] tutorial lvl

--- src/menu/menu.gd
+++ src/menu/menu.gd
@@ -11,7 +11,7 @@ func _process(delta):
 	pass
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://src/world.tscn")
+	get_tree().change_scene_to_file("res://src/HowToMoove.tscn")
 
 func _on_options_button_pressed():
 	open_option = !open_option
